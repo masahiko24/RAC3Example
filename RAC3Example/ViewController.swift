a777ba406db6aac61d3fc15f7b311e5e0af49f91
Masahiko Tsujita <masahikot.uec@icloud.com>
2015-07-25 17:12:40 +0900

    Initial Commit

---
+++ RAC3Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RAC3Example
+//
+//  Created by 辻田晶彦 on 2015/07/25.
+//  Copyright (c) 2015年 Masahiko Tsujita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
